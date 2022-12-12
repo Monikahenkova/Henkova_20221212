abe5b82527713cfbcdf9f8463f6610a9172e9dfc
Monika Hénková <Henkova.Monika@stud.vassboskovice.cz>
2022-12-12 13:42:09 +0100

    Add project files.

---
+++ Henkova_20221212/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Henkova_20221212
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Soldier jedna;
+        Soldier dva;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            jedna = new Soldier();
+            jedna.Name = "Jedna";
+            Zobraz(jedna, txt1);
+
+            dva = new Soldier();
+            dva.Name = "Dva";
+            Zobraz(dva, txt2);
+        }
+
+        public void Zobraz(Soldier soldier, TextBox textBox)
+        {
+            textBox.Text = soldier.Name + "\n";
+            textBox.Text += "HP: " + soldier.HP.ToString() + "\n";
+            textBox.Text += "Shield: " + soldier.Shield.ToString() + "\n";
+            textBox.Text += "Level: " + soldier.Level.ToString() + "\n";
+      
+        }
+
+
+        private void Button_Click0(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Henkova_20221212/Soldier.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Henkova_20221212
+{
+    public class Soldier
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int DMG { get; set; }
+        public int Shield { get; set; }
+        public int MaxShield { get; set; }
+        public int Level { get; set; }
+        public string Attack { get; set; }
+        public string Heal { get; set; }
+        public int LevelUp { get; set; }
+
+        public Soldier()
+        {
+            Name = "";
+            HP = 100;
+            MaxHP = 100;
+            DMG = 1;
+            Shield = 100;
+            MaxShield = 100;
+            Level = 1;
+
+        }
+
+        public void Bojovat()
+        {
+            if ()
+        }
+
+        public void ZvysitLevel()
+        {
+
+        }
+
+        public void LecitSe()
+        {
+
+        }
+    }
+
+}
